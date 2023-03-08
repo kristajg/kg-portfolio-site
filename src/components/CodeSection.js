98355a5ba8c056660d88db355f232326726d9bb2
kristajg <krista.goralczyk@gmail.com>
2023-03-08 16:54:14 -0700

    Small changes

--- src/components/CodeSection.js
+++ src/components/CodeSection.js
@@ -7,13 +7,13 @@ export default function CodeSection(){
           <b>Twilio Demo Platform</b> is a <a href='https://github.com/kristajg/kg-demo-app' target='_blank'>frontend</a> and <a href='https://github.com/kristajg/kg-demo-api' target='_blank'>API</a> showcasing Twilio's core communications products. Includes SMS, soft phone, multi-channel 2FA.
         </p>
         <p>
-          <a href='https://github.com/kristajg/pburgh-phaser' target='_blank'>Pigeonburgh</a>, a top-down RPG game built using Phaser.io. Enjoy friendship, bridges, and (obviously) pigeons.
+          <a href='https://github.com/kristajg/pburgh-phaser' target='_blank'>Pigeonburgh</a>, a <b>top-down RPG game</b> built using Phaser.io. Enjoy friendship, bridges, and (obviously) pigeons.
         </p>
         <p>
-          <a href='https://github.com/kristajg/cozy-holiday' target='_blank'>Cozy Holiday</a> is an npm package built with React. Features holiday themed pixel art to add to your website.
+          <a href='https://github.com/kristajg/cozy-holiday' target='_blank'>Cozy Holiday</a> is an <b>npm package</b> built with React. Features holiday themed pixel art to add to your website.
         </p>
         <p>
-          <a href='https://github.com/kristajg/react-aws-audio-transcription' target='_blank'>Real time audio transcription</a> using AWS Transcribe and React. Converts audio from a browser into text automatically.
+          <a href='https://github.com/kristajg/react-aws-audio-transcription' target='_blank'>Real time audio transcription</a> using AWS Transcribe and React. <b>Converts audio into text</b> as you speak.
         </p>
       </div>
     </div> 
