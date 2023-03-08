bc9f9c9c52c5d1078d71ec0cf59a13245e59b4d8
kristajg <krista.goralczyk@gmail.com>
2023-03-08 15:46:13 -0700

    typo

--- src/components/WritingSection.js
+++ src/components/WritingSection.js
@@ -4,7 +4,7 @@ export default function WritingSection(){
       <div className='content-header'>writing</div>
       <div>
         <p>
-          Twilio blog post on <a href='https://www.twilio.com/blog/add-custom-attributes-twilio-sms-campaigns' target='_blank'>attaching metadata to Prgrammable Messaging</a>
+          Twilio blog post on <a href='https://www.twilio.com/blog/add-custom-attributes-twilio-sms-campaigns' target='_blank'>attaching metadata to Programmable Messaging</a>
         </p>
         <p>
           Twilio blog post on <a href='https://www.twilio.com/blog/passing-context-between-google-dialogflow-and-twilio-studio' target='_blank'>transferring data between IVR phone systems and Google Dialogflow</a>.
