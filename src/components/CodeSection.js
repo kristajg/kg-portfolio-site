101038ad05ebe3590771574adebcd92515059caf
kristajg <krista.goralczyk@gmail.com>
2023-03-08 15:11:21 -0700

    code project content

--- src/components/CodeSection.js
+++ src/components/CodeSection.js
@@ -2,7 +2,20 @@ export default function CodeSection(){
   return (
     <div className='content-section'  id='section-code'>
       <div className='content-header'>code</div>
-      List code proj here
+      <div>
+        <p>
+          <b>Twilio Demo Platform</b> is a <a href='https://github.com/kristajg/kg-demo-app' target='_blank'>frontend</a> and <a href='https://github.com/kristajg/kg-demo-api' target='_blank'>API</a> showcasing Twilio's core communications products. Includes SMS, soft phone, multi-channel 2FA.
+        </p>
+        <p>
+          <a href='https://github.com/kristajg/pburgh-phaser' target='_blank'>Pigeonburgh</a>, a top-down RPG game built using Phaser.io. Enjoy friendship, bridges, and (obviously) pigeons.
+        </p>
+        <p>
+          <a href='https://github.com/kristajg/cozy-holiday' target='_blank'>Cozy Holiday</a> is an npm package built with React. Features holiday themed pixel art to add to your website.
+        </p>
+        <p>
+          <a href='https://github.com/kristajg/react-aws-audio-transcription' target='_blank'>Real time audio transcription</a> using AWS Transcribe and React. Converts audio from a browser into text automatically.
+        </p>
+      </div>
     </div> 
   )
 }
