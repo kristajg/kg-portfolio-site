5e78c9f9e1124683a6de498d80e2195cd82ce680
kristajg <krista.goralczyk@gmail.com>
2023-04-13 13:37:27 -0600

    Add grid section for speaking

--- src/components/SpeakingSection.js
+++ src/components/SpeakingSection.js
@@ -3,18 +3,27 @@ export default function SpeakingSection(){
     <div className='content-section alternate-section-background'  id='section-speaking'>
       <div className='content-header'>speaking</div>
       <div className='content-body'>
-        {/* <div className='container'>
-          <div className='row'>
-            <div className='col-sm'>
+        <div className='grid-wrapper'>
+            <div className='grid-item'>
               <a href='https://www.youtube.com/watch?v=OUraWc_DN1Q' target='_blank' rel="noreferrer">Twilio SIGNAL 2022 - Five best practices for a conversational IVR</a>
             </div>
+            <div className='grid-item'>
+              Presenter for Women who Code ATX
+            </div>
+            <div className='grid-item'>
+              Panelist for Envestnet's Women Leaders in Finance
+            </div>
+            <div className='grid-item'>
+              Interviewee for Reprise Solutions Engineer Interview Series
+            </div>
+            <div className='grid-item'>
+              Oscars Tech Javascript Meetup 2022 - Presented Phaser.io Game Dev Basics
+            </div>
+            <div className='grid-item'>
+              Mortified Live Comedy Show - Austin, Texas
+            </div>
           </div>
-        </div> */}
-
-        <p>
-          Twilio SIGNAL 2022, Women who Code ATX, Envestnet's Women Leaders in Finance, Reprise Solutions Engineer Interview Series, Oscars Tech Javascript Meetup 2022 - Presented Phaser.io Game Dev Basics, and the Mortified Live - Austin comedy show.
-        </p>
-      </div>
+        </div>
     </div> 
   )
 }
