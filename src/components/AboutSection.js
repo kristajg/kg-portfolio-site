8954a12e3f39fc2f8f8a47544800a00dea296dcb
kristajg <krista.goralczyk@gmail.com>
2025-11-16 17:12:21 -0500

    update links in about me

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -15,12 +15,12 @@ export default function AboutSection(){
           <p className='bio-paragraph'>
             Currently I&#39;m <b>based in the DC area</b>, but am <b>originally from Pittsburgh</b>. Yes, I am familiar with pierogi both as a food (delicious) and as a mascot (❤️ Jalapeno Hannah).
             <br/><br/>
-            I am a <b>solutions architect &#38; programmer</b> who believes tech products should be performant, secure, pragmatic, and exciting. For inquiries regarding technical consultation, web development, or speaking opportunities <a href="mailto:krista.goralczyk@gmail.com">drop me a line</a>. My resume is available here.
+            As a <b>solutions architect &#38; programmer</b>, I believe tech products should be performant, secure, and most importantly, pragmatic. For inquiries regarding technical consultation, web development, or speaking opportunities <a href="mailto:krista.goralczyk@gmail.com">drop me a line</a>. Find my <a href={resume} target='_blank' rel="noreferrer">resume here</a>.
             <br/><br/>
             When I'm not at work, you can find me reading through the huge stack of books that is constantly replenished and therefore never gets any smaller. With a sundry of interests (anything from making latch hook rugs to weight lifting) I'm always working on a few projects. Find more projects and writing below.
             <br/><br/>
             <b>Latest Side Projects:</b><br/>
-            • The Hungry Seagull - easy to play Pico-8 video game<br/>
+            • <a href='https://www.lexaloffle.com/bbs/?tid=150987' target='_blank' rel="noreferrer">The Hungry Seagull</a> - Pico-8 video game<br/>
             • maintaining my compost bin - AKA earthworm metropolis<br/>
             • TBD short story - non-dystopian futuristic fiction
           </p>
