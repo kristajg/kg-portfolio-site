92c4056fb91a263e4a09a4b146bea0b5018b2a50
kristajg <krista.goralczyk@gmail.com>
2025-11-16 16:01:01 -0500

    add prettier, small text change in about

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -13,7 +13,7 @@ export default function AboutSection(){
             <img src={kg} className='bio-img' alt='Krista' />
           </div>
           <p className='bio-paragraph'>
-            Howdy! I am a <b>solutions engineer &#38; programmer</b> with over 12 years of tech industry experience. I'm driven to build new things with creativity and positive intent.<br /><br />
+            Howdy! I am a <b>solutions engineer &#38; programmer</b> with over 14 years of tech industry experience. I'm driven to build new things with creativity and positive intent.<br /><br />
             I know the full gamut of what it takes to conceptualize &#38; build a fullstack product - then take it to market. My preferred code stack is <b>React + Node.js</b>.<br /><br />
             Currently, <b>I am available for hire or freelance work</b>. For inquiries regarding technical consultation, web development, or speaking opportunities <a href="mailto:krista.goralczyk@gmail.com">drop me a line</a>. My resume is <a href={resume} target='_blank' rel="noreferrer">available here</a>.
           </p>
