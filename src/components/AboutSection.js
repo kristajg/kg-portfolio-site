c2d0239a3832305e9716ee32c4cae361aa6d3387
krista <krista@kristas-MacBook-Pro.local>
2026-02-11 13:57:50 -0500

    small copy change

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -12,7 +12,7 @@ export default function AboutSection(){
             <img src={kg} className='bio-img' alt='Krista' />
           </div>
           <p className='bio-paragraph'>
-            I&#39;m <b>based in Pittsburgh</b>, and yes: I am familiar with the concept of pierogi both as a food (🥟 delicious) and as a mascot (💚 Jalapeno Hannah).
+            My name is Krista and I&#39;m <b>based in Pittsburgh</b>, so I am familiar with the concept of pierogi both as a food (🥟 delicious) and as a mascot (💚 Jalapeno Hannah).
             <br/><br/>
             I like writing and making video games. Right now I'm working on a little bit of both. Play my latest Pico-8 game <a href='https://www.lexaloffle.com/bbs/?tid=150987' target='_blank' rel="noreferrer">The Hungry Seagull</a>. Will you score high enough to be crowned the fry queen?
             <br/><br/>
