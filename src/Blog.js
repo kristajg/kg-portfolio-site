b97f3215fec94c78e76a2778bd8badb0f33d879c
krista <krista@mac.mynetworksettings.com>
2026-06-15 16:42:16 -0400

    add basic blog route and style

---
+++ src/Blog.js
@@ -0,0 +1,14 @@
+import { Footer } from './components/Footer';
+
+function Blog() {
+  return (
+    <div className='Blog'>
+        <div className='blog-content'>
+            Blog goes here
+        </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Blog;
